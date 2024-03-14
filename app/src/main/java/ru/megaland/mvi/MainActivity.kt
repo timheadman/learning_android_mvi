ef7ed99511c1323d2a24dbf832ba66a79a290929
Tim <timhdm@ya.ru>
2024-03-14 16:44:52 +0300

    minor

--- app/src/main/java/ru/megaland/mvi/MainActivity.kt
+++ app/src/main/java/ru/megaland/mvi/MainActivity.kt
@@ -1,10 +1,15 @@
 package ru.megaland.mvi
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,6 +19,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
@@ -24,14 +30,20 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ru.megaland.mvi.ui.theme.MviTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,18 +54,11 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    Column {
-                        Row {
-                            ListItem(name = "Arnold", profession = "Actor")
-                        }
-                        Row {
-                            ListItem(name = "Arnold", profession = "Actor")
-                        }
-                        Row {
-                            ListItem(name = "Arnold", profession = "Actor")
-                        }
-                        Row {
-                            ListItem(name = "Arnold", profession = "Actor")
+                    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                        repeat(15) {
+                            Row {
+                                ListItem(name = "Arnold", number = it, profession = "Actor")
+                            }
                         }
                     }
                 }
@@ -63,16 +68,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun ListItem(name: String, profession: String) {
+private fun ListItem(name: String, number: Int, profession: String) {
+    val counter = remember {
+        mutableIntStateOf(0)
+    }
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(10.dp),
+            .padding(10.dp)
+            .clickable {
+                counter.intValue++
+                Log.d("AAA", "ListItem $name$number Click! ")
+            }
+            .pointerInput(Unit) {
+                detectDragGesturesAfterLongPress { change, dragAmount ->
+                    Log.d(
+                        "AAA",
+                        "ListItem $name$number detectDragGesturesAfterLongPress:  $dragAmount ${change.position}"
+                    )
+                }
+            },
         shape = RoundedCornerShape(20.dp),
         colors = CardDefaults.cardColors(MaterialTheme.colorScheme.background),
-        elevation = CardDefaults.cardElevation(2.dp),
-
-        ) {
+        elevation = CardDefaults.cardElevation(2.dp)
+    ) {
         Box {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
@@ -87,10 +106,15 @@ private fun ListItem(name: String, profession: String) {
                         .clip(CircleShape)
                 )
                 Column(modifier = Modifier.padding(start = 10.dp)) {
-                    Text(text = name)
-                    Text(text = profession)
+                    Text(text = "$name$number", fontWeight = FontWeight.Bold)
+                    Text(text = "$profession$number")
                 }
-
+                Column(
+                    modifier = Modifier.background(Color.Red),
+                    horizontalAlignment = Alignment.End
+                ) {
+                    Text(text = "${counter.intValue}", color = MaterialTheme.colorScheme.background, fontSize = 25.sp, fontWeight = FontWeight.Bold)
+                }
             }
         }
     }
