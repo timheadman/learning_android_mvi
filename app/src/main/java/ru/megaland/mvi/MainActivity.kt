2d7b4aa708effc4d4c8d5cb570a1d7abf9c3ac77
Tim <timhdm@ya.ru>
2024-03-15 09:18:17 +0300

    ColumnListItem release

--- app/src/main/java/ru/megaland/mvi/MainActivity.kt
+++ app/src/main/java/ru/megaland/mvi/MainActivity.kt
@@ -7,14 +7,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -25,13 +22,11 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -41,7 +36,6 @@ import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ru.megaland.mvi.ui.theme.MviTheme
@@ -57,7 +51,7 @@ class MainActivity : ComponentActivity() {
                     Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
                         repeat(15) {
                             Row {
-                                ListItem(name = "Arnold", number = it, profession = "Actor")
+                                ColumnListItem(name = "Arnold", number = it, profession = "Actor")
                             }
                         }
                     }
@@ -68,7 +62,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun ListItem(name: String, number: Int, profession: String) {
+private fun ColumnListItem(name: String, number: Int, profession: String) {
     val counter = remember {
         mutableIntStateOf(0)
     }
@@ -110,10 +104,17 @@ private fun ListItem(name: String, number: Int, profession: String) {
                     Text(text = "$profession$number")
                 }
                 Column(
-                    modifier = Modifier.background(Color.Red),
-                    horizontalAlignment = Alignment.End
+                    modifier = Modifier
+                        .background(Color.Red, shape = CircleShape)
+                        .padding(horizontal = 10.dp, vertical = 2.dp),
                 ) {
-                    Text(text = "${counter.intValue}", color = MaterialTheme.colorScheme.background, fontSize = 25.sp, fontWeight = FontWeight.Bold)
+
+                    Text(
+                        text = "${counter.intValue}",
+                        color = MaterialTheme.colorScheme.background,
+                        fontSize = 25.sp,
+                        fontWeight = FontWeight.Bold
+                    )
                 }
             }
         }
